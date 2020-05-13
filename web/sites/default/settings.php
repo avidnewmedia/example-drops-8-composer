22c6aa7dc4b24f2aaa5d2233374cea8ef62bc826
Dustin Harrell <dustin@avidnewmedia.io>
2020-05-13 15:41:16 -0400

    removed db connection string from settings.php as it isnt needed

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -30,20 +30,3 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-
-/**
- * Always install the 'standard' profile to stop the installer from
- * modifying settings.php.
- */
-$settings['install_profile'] = 'standard';
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'iiisaGdY9LQED3uaW7ARXKcIEDaVuo2jjOuR5pXATgTdKKApfouLnrnIGkmiiqqJr3owkXZhlA';
