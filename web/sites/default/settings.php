4573fdffa83b6cd7638beca4b03d9b4f3f8e6420
Dustin Harrell <dustin@avidnewmedia.io>
2020-05-14 14:42:47 -0400

    updates to settings.php to include changes to config defaults

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -19,9 +19,12 @@ include __DIR__ . "/settings.pantheon.php";
 /**
  * Place the config directory outside of the Drupal root.
  */
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
-);
+$settings['config_sync_directory'] = DRUPAL_ROOT . '/../config/sync';
+
+// Automatically enable the correct config_split based on the Pantheon environment.
+$_env_is_live = isset($_ENV['PANTHEON_ENVIRONMENT']) && ($_ENV['PANTHEON_ENVIRONMENT'] == 'live' || $_ENV['PANTHEON_ENVIRONMENT'] == 'test');
+$config['config_split.config_split.dev']['status'] = !$_env_is_live;
+$config['config_split.config_split.live']['status'] = $_env_is_live;
 
 /**
  * If there is a local settings file, then include it
@@ -30,3 +33,10 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+/**
+ * Always install the 'standard' profile to stop the installer from
+ * modifying settings.php.
+ */
+$settings['install_profile'] = 'standard';
+
